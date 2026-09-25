7688b10dee20af14e59d2b6d703d71343c8b05b8
sameergouda1020-hue <sameergouda1020@gmil.com>
2026-09-25 12:42:48 +0530

    assiggnment

---
+++ Sept/week4/Day4/task/script.js
@@ -0,0 +1,74 @@
+let form = document.getElementById("studentForm");
+
+form.style.width = "400px";
+form.style.padding = "20px";
+form.style.border = "2px solid black";
+form.style.borderRadius = "10px";
+form.style.backgroundColor = "aqua";
+form.style.margin = "auto";
+form.style.justifyItems ="center"
+
+let heading = document.getElementById("heading");
+ heading.style.fontWeight = "5px"
+
+let name = document.getElementById("name");
+
+name.style.width = "300px";
+name.style.height = "30px";
+name.style.border = "1px solid black";
+
+
+let email = document.getElementById("email");
+
+email.style.width = "300px";
+email.style.height = "30px";
+email.style.border = "1px solid black";
+
+
+let phone = document.getElementById("phone");
+
+phone.style.width = "300px";
+phone.style.height = "30px";
+phone.style.border = "1px solid black";
+
+
+let age = document.getElementById("age");
+
+age.style.width = "300px";
+age.style.height = "30px";
+age.style.border = "1px solid black";
+
+
+let course = document.getElementById("course");
+
+course.style.width = "305px";
+course.style.height = "35px";
+course.style.border = "1px solid black";
+
+
+let password = document.getElementById("password");
+
+password.style.width = "300px";
+password.style.height = "30px";
+password.style.border = "1px solid black";
+
+
+let confirmPassword =
+    document.getElementById("confirmPassword");
+
+confirmPassword.style.width = "300px";
+confirmPassword.style.height = "30px";
+confirmPassword.style.border = "1px solid black";
+
+
+let registerButton =
+ document.getElementById("registerButton");
+
+registerButton.style.width = "120px";
+registerButton.style.height = "40px";
+registerButton.style.backgroundColor = "blue";
+registerButton.style.color = "white";
+registerButton.style.border = "none";
+registerButton.style.borderRadius = "5px";
+
+
